a76d4293f40beaf6a099cc440a6e41602ba10765
happyeveryone96 <happyeveryone@naver.com>
2022-07-13 20:49:38 +0900

    Add: 로그인 유효성 검사 기능 추가

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,39 +1,23 @@
 import React from 'react';
 import css from './Header.module.scss';
-import { FiSearch } from "react-icons/fi";
+import { FiSearch } from 'react-icons/fi';
 
 function Header() {
   return (
     <>
       <div className={css.top}>
-        <div className={css.login}>
-          로그인
-        </div>
-        <div className={css.mypage}>
-          마이페이지
-        </div>
-        <div className={css.interest}>
-          관심상품
-        </div>
-        <div className={css.customer_center}>
-          고객센터
-        </div>
+        <div className={css.login}>로그인</div>
+        <div className={css.mypage}>마이페이지</div>
+        <div className={css.interest}>관심상품</div>
+        <div className={css.customer_center}>고객센터</div>
       </div>
       <div className={css.second_border}>
-        <div className={css.kream_log}>
-            KREAM
-        </div>
-        <div className={css.sTyle}>
-          STYLE
-        </div>
-        <div className={css.shop}>
-          SHOP
-        </div>
-        <div className={css.about}>
-         ABOUT
-        </div>
+        <div className={css.kream_log}>KREAM</div>
+        <div className={css.sTyle}>STYLE</div>
+        <div className={css.shop}>SHOP</div>
+        <div className={css.about}>ABOUT</div>
         <div className={css.glass}>
-             <FiSearch size="35"/>
+          <FiSearch size="35" />
         </div>
       </div>
     </>
