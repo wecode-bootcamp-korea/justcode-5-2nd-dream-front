4997005846e66891871dee0b7b9067c74c371232
유관희 <kwan@yugwanhuiui-MacBookPro.local>
2022-07-15 00:55:00 +0900

    main 화면 첫번째 베너광고 슬라이드로 구현

---
+++ src/pages/Home/Banner.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import css from "./Banner.module.scss";
+import b1 from './images/b1.png';
+import b2 from './images/b2.png';
+import b3 from './images/b3.png';
+import b4 from './images/b4.png';
+import b5 from './images/b5.png';
+import b6 from './images/b6.png';
+
+export default class SimpleSlider extends Component {
+    render() {
+        const settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        };
+        return (
+            <div>
+
+                <Slider {...settings}>
+                    <div>
+                        <img className={css.banner} src={b1} />
+                    </div>
+                    <div>
+                        <img className={css.banner} src={b2} />
+                    </div>
+                    <div>
+                        <img className={css.banner} src={b3} />
+                    </div>
+                    <div>
+                        <img className={css.banner} src={b4} />
+                    </div>
+                    <div>
+                        <img className={css.banner} src={b5} />
+                    </div>
+                    <div>
+                        <img className={css.banner} src={b6} />
+                    </div>
+                </Slider>
+            </div>
+        );
+    }
+}
