397695727755d61031fa4c53ed62324684292bf3
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2022-07-15 01:16:03 +0900

    Add: 제품 상세 페이지 UI 및 사이즈 모달 UI 구현 (#21)

---
+++ src/components/ProductModal/ProductModal.js
@@ -0,0 +1,54 @@
+import React, { useEffect } from 'react';
+import css from './ProductModal.module.scss';
+
+const ProductModal = props => {
+  const { open, close, size, setSize, setPrice } = props;
+
+  useEffect(() => {
+    open
+      ? (document.body.style.overflow = 'hidden')
+      : (document.body.style.overflow = 'unset');
+  }, [open]);
+
+  const handleSize = e => {
+    setSize(e.target.value?.split(',')[0]);
+    setPrice(Number(e.target.value?.split(',')[1]));
+    close();
+  };
+  const sizeList = ['모든 사이즈', 'S', 'M', 'L', 'XL', 'XXL'];
+  const priceList = [60000, 66000, 90000, 45000, 100000, 50000, 40000];
+
+  return (
+    <div className={open ? `${css.openModal} ${css.modal}` : css.modal}>
+      {open ? (
+        <section>
+          <header>
+            <div>사이즈</div>
+            <button className={css.confirm} onClick={close}>
+              X
+            </button>
+          </header>
+          <div className={css.sizes}>
+            {sizeList?.map((s, idx) => {
+              const isSelected = s === size;
+              return (
+                <button
+                  className={isSelected ? css.selected : undefined}
+                  onClick={handleSize}
+                  value={[s, priceList[idx]]}
+                  key={s}
+                >
+                  {s}
+                  <br />
+                  {priceList[idx]}
+                </button>
+              );
+            })}
+          </div>
+        </section>
+      ) : null}
+    </div>
+  );
+};
+
+export default ProductModal;
--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import ProductImage from '../../components/ProductImage/ProductImage';
+import ProductInfo from '../../components/ProductInfo/ProductInfo';
+import ProductStyle from '../../components/ProductStyle/ProductStyle';
+import OtherProduct from '../../components/OtherProduct/OtherProduct';
+import css from './ProductDetail.module.scss';
 
 function ProductDetail() {
-  return <div>ProductDetail</div>;
+  return (
+    <div className={css.container}>
+      <div className={css.info_container}>
+        <ProductImage />
+        <ProductInfo />
+      </div>
+      <h2>스타일</h2>
+      <div className={css.style_container}>
+        <ProductStyle />
+        <ProductStyle />
+        <ProductStyle />
+        <ProductStyle />
+        <ProductStyle />
+        <ProductStyle />
+      </div>
+      <h2>Aurolee의 다른 상품</h2>
+      <div className={css.other_container}>
+        <OtherProduct />
+        <OtherProduct />
+        <OtherProduct />
+        <OtherProduct />
+        <OtherProduct />
+        <OtherProduct />
+      </div>
+    </div>
+  );
 }
 
 export default ProductDetail;
