bdd3f53e540b3dfd94890b8c533212b5e4e33a7a
유관희 <kwan@yugwanhuiui-MacBookPro.local>
2022-07-15 23:06:54 +0900

    mock db를 이용한 품목

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import Banner from './Banner'
-
+import Banner from './Banner';
+import Post from './Post';
+import Slide from './Slide';
+import ProductAll from './ProductAll';
 function Home() {
   return <div>
     <Banner />
+    <Post />
+    {/* <Slide /> */}
+    <ProductAll />
   </div>;
 }
 
