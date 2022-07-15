cce05f919f925602e9676694be69786d07c9886a
happyeveryone96 <happyeveryone@naver.com>
2022-07-15 18:55:44 +0900

    Add: 배송 및 결제 페이지 UI 구현

--- src/pages/Router.js
+++ src/pages/Router.js
@@ -14,6 +14,7 @@ import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 import DealCheck from './DealCheck/DealCheck';
 import BeforePayment from './BeforePayment/BeforePayment';
+import Payment from './Payment/Payment';
 
 function App() {
   return (
@@ -35,6 +36,7 @@ function App() {
         <Route path="/sell/check/:id" element={<DealCheck />} />
         <Route path="/buy/:id" element={<BeforePayment />} />
         <Route path="/sell/:id" element={<BeforePayment />} />
+        <Route path="/payment/:id" element={<Payment />} />
       </Routes>
       <Footer />
     </BrowserRouter>
