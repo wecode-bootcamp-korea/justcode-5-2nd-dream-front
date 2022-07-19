eeab24b7fa3eb32aa6b9915bcb42aa3fba20a5dd
Sophie.S <100933263+Sophie0527@users.noreply.github.com>
2022-07-19 12:55:36 +0900

    Add: 백엔드 API를 활용하여 프로필 조회구현 (#42)

--- src/components/Profile/ProfileInfo.js
+++ src/components/Profile/ProfileInfo.js
@@ -1,51 +1,30 @@
-import React, { useState, useRef } from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useState, useRef, useEffect } from 'react';
 import css from './ProfileInfo.module.scss';
-import SizeModal from '../../components/SizeModal/SizeModal';
+// import SizeModal from '../../components/SizeModal/SizeModal';
 
-function ProfileInfo() {
-  const id = useLocation().pathname.split('/')[2] - 1;
-  const userData = [
-    {
-      id: 1,
-      image:
-        'https://i.pinimg.com/564x/7b/cf/9f/7bcf9fd06731087ef9edce2a35fd2cfa.jpg',
-      email: 'ssh3051004@naver.com',
-      password: '1234567',
-      name: '김드림',
-      phone: '010-0000-1111',
-      size: '240',
-    },
-    {
-      id: 2,
-      image:
-        'https://media.istockphoto.com/photos/flat-white-knitwear-sweater-tshirt-shape-with-red-line-on-the-rim-on-picture-id1403388731?s=612x612',
-      email: 'aa123@naver.com',
-      password: '1234567',
-      name: '이드림',
-      phone: '010-0000-2222',
-      size: '250',
-    },
-    {
-      id: 3,
-      image:
-        'https://media.istockphoto.com/photos/pullover-isolated-on-white-background-picture-id1160547812?s=612x612',
-      email: 'ssh3051004@naver.com',
-      password: '1234567',
-      name: '박드림',
-      phone: '010-0000-3333',
-      size: '245',
-    },
-  ];
-  const [size, setSize] = useState(undefined);
-  const [modalOpen, setModalOpen] = useState(false);
+function ProfileInfo(props) {
+  const { profileInfo, id, setProfileInfo } = props;
+  useEffect(() => {
+    fetch(`http://localhost:10010/mypage/${id}`, {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(data => {
+        setProfileInfo(data.data[0]);
+      });
+  }, [id, setProfileInfo]);
 
-  const openModal = () => {
-    setModalOpen(true);
-  };
-  const closeModal = () => {
-    setModalOpen(false);
-  };
+  //============ 신방사이즈 모달창 부분
+
+  // const [size, setSize] = useState(undefined);
+  // const [modalOpen, setModalOpen] = useState(false);
+
+  // const openModal = () => {
+  //   setModalOpen(true);
+  // };
+  // const closeModal = () => {
+  //   setModalOpen(false);
+  // };
 
   //============ 이름 변경 부분
 
@@ -97,12 +76,12 @@ function ProfileInfo() {
 
   return (
     <div className={css.content_box}>
-      <SizeModal
+      {/* <SizeModal
         close={closeModal}
         open={modalOpen}
         size={size}
         setSize={setSize}
-      />
+      /> */}
 
       <div className={css.my_profile}>
         <div className={css.content_title_border}>
@@ -110,9 +89,9 @@ function ProfileInfo() {
         </div>
         <div className={css.user_profile}>
           <div className={css.profile_img}>
-            <img src={userData[id].image} alt="사용자이미지" />
+            <img src={profileInfo.image} alt="사용자이미지" />
           </div>
-          <h3>{userData[id].name}</h3>
+          <h3>{profileInfo.name}</h3>
         </div>
         <div className={css.profile_info}>
           <div className={css.profile_group}>
@@ -120,20 +99,13 @@ function ProfileInfo() {
             <div className={css.email_box}>
               <div className={css.email_info}>
                 <h5>이메일 주소</h5>
-                <p>{userData[id].email}</p>
+                <p>{profileInfo.email}</p>
               </div>
-              <button
-                onClick={() =>
-                  alert('소셜 로그인된 상태에서는 이메일을 변경할 수 없습니다.')
-                }
-              >
-                변경
-              </button>
             </div>
             <div className={css.email_box}>
               <div className={css.email_info}>
                 <h5>비밀번호</h5>
-                <p>●●●●●●●●●</p>
+                <p>{profileInfo.password}</p>
               </div>
               <button
                 onClick={() =>
@@ -153,7 +125,7 @@ function ProfileInfo() {
               <div className={css.email_box}>
                 <div className={css.email_info}>
                   <h5>이름</h5>
-                  <p>{userData[id].name} </p>
+                  <p>{profileInfo.name} </p>
                 </div>
                 <button onClick={openName}>변경</button>
               </div>
@@ -175,17 +147,18 @@ function ProfileInfo() {
             <div className={css.email_box}>
               <div className={css.email_info}>
                 <h5>휴대폰 번호</h5>
-                <input placeholder={userData[id].phone} />
+                <input placeholder={profileInfo.phone} />
               </div>
               <button>변경</button>
             </div>
-            <div className={css.email_box}>
+            {/* 신발사이즈 일단 주석처리 해놓기! */}
+            {/* <div className={css.email_box}>
               <div className={css.email_info}>
                 <h5>신발 사이즈</h5>
                 <p>{userData[id].size}</p>
               </div>
               <button onClick={openModal}>변경</button>
-            </div>
+            </div> */}
           </div>
           <span>회원탈퇴</span>
         </div>
@@ -213,8 +186,8 @@ function ProfileInfo() {
               return (
                 <div key={list.id} className={css.add_lists}>
                   <div>
-                    <p>{userData[id].name}</p>
-                    <p>{userData[id].phone}</p>
+                    <p>{profileInfo.name}</p>
+                    <p>{profileInfo.phone}</p>
                     <p>{list.text}</p>
                     {addressOpened && (
                       <div>
