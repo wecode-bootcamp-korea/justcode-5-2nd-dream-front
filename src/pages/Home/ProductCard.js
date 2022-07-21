0e9278b71367fabc972278c9587c9ab740685120
유관희 <kwan@yugwanhuiui-MacBookPro.local>
2022-07-21 17:10:26 +0900

    fix

--- src/pages/Home/ProductCard.js
+++ src/pages/Home/ProductCard.js
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from 'react';
 import css from './ProductCard.module.scss';
 const ProductCard = ({ item }) => {
-    return (
-        <div>
-            <img className={css.pic} src={item?.productImg} alt="img"/>
-            <div className={css.nam}>{item?.productName}</div>
-            <div className={css.com}>{item?.coment}</div>
-            <div className={css.pri}>{item?.price} 원</div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <img className={css.pic} src={item?.productImg} alt="img" />
+      <div className={css.nam}>{item?.productName}</div>
+      <div className={css.com}>{item?.coment}</div>
+      <div className={css.pri}>{item?.price} 원</div>
+    </div>
+  );
+};
 
-export default ProductCard
+export default ProductCard;
