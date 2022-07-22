91b868204b39c0313da9fc48fdf462aa2f90c048
KIMYECHAN <kyc5244@gmail.com>
2022-07-22 10:35:56 +0900

    develop Merge

--- src/pages/Home/Slide.js
+++ src/pages/Home/Slide.js
@@ -4,16 +4,6 @@ import ProductCard from './ProductCard';
 import ProductAll from './ProductAll';
 import { Container, Row, Col } from 'react-bootstrap';
 import css from './Slide.module.scss';
-import s1 from './images/s1.png';
-import s2 from './images/s2.png';
-import s3 from './images/s3.png';
-import s4 from './images/s4.png';
-import s5 from './images/s5.png';
-import s6 from './images/s6.png';
-import s7 from './images/s7.png';
-import s8 from './images/s8.png';
-import s9 from './images/s9.png';
-import s10 from './images/s10.png';
 
 export default class SwipeToSlide extends Component {
   render() {
