7bf60baa5914b4d41fd4ffaaec51ae4f756067d4
손소희 <ssh30510044@gmail.com>
2022-07-28 09:55:30 +0900

    Refactor: shop 페이지 관심상품 상태 가져오기

---
+++ src/components/ShopSearchContent/ShopWishButton.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import css from './ShopWishButton.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBookmark } from '@fortawesome/free-solid-svg-icons';
+import BASE_URL from '../../config';
+
+function ShopWishButton(props) {
+  const { searchInfo } = props;
+
+  const [isUpdated, setIsUpdated] = useState(false);
+  const [isWished, setIsWished] = useState(undefined);
+
+  useEffect(() => {
+    const userId = localStorage.getItem('userId');
+    setIsUpdated(false);
+    fetch(`${BASE_URL}/main/wish/${userId}`, {
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(data => {
+        const wishLength = data.data.filter(
+          wishInfo => wishInfo.product_id === Number(searchInfo.id)
+        ).length;
+        if (wishLength !== 0) {
+          setIsWished(true);
+        } else if (wishLength === 0) {
+          setIsWished(false);
+        }
+      });
+  }, [isUpdated, isWished, searchInfo.id]);
+
+  return (
+    <div className={css.interest_figure}>
+      <button className={css.wish_figure}>
+        {isWished !== undefined && (
+          <FontAwesomeIcon
+            icon={faBookmark}
+            className={css.bookmark}
+            color={isWished ? 'black' : 'lightgray'}
+          />
+        )}
+        <p>{searchInfo.wish === null ? 0 : searchInfo.wish}</p>
+      </button>
+    </div>
+  );
+}
+
+export default ShopWishButton;
